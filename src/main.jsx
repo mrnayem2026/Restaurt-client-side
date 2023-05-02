969b29c2d801420a2ef8131bdb8efddb3e4d1d37
Mostafizur Rahman nayem <hpbd17062022@gmail.com>
2023-05-02 17:49:25 +0600

    API Context done

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import './index.css'
 import router from './routes/Routes';
+import AuthProviders from './providers/AuthProviders/AuthProviders';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProviders>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProviders>
   </React.StrictMode>,
 )
